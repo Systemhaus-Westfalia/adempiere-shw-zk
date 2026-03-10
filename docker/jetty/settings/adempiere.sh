2531e74c1f9e24a458f3e55713f72f011d13bd71
marcalwestf <mariocalderon007@gmail.com>
2026-03-10 15:09:46 -0600

    Update adempiere-shw to 3.9.4.001-1.1.50, Java 17, fix Byte Buddy and SLF4J conflicts

--- docker/jetty/settings/adempiere.sh
+++ docker/jetty/settings/adempiere.sh
@@ -36,7 +36,6 @@ sed -i "s|@ADEMPIERE_DB_PASSWORD@|$ADEMPIERE_DB_PASSWORD|g" $JETTY_BASE/jetty-ds
 
 
 sed -i "s|/usr/local/jetty/etc/jetty-http.xml|/usr/local/jetty/etc/jetty-http.xml /var/lib/jetty/jetty-ds.xml|g" $JETTY_BASE/jetty.start
-sed -i "s|-Djetty.base=/var/lib/jetty|-Djetty.base=/var/lib/jetty -DADEMPIERE_HOME=$ADEMPIERE_HOME|g" $JETTY_BASE/jetty.start
+sed -i "s|-Djetty.base=/var/lib/jetty|-Djetty.base=/var/lib/jetty -DADEMPIERE_HOME=$ADEMPIERE_HOME -Dorg.eclipse.jetty.LEVEL=ALL|g" $JETTY_BASE/jetty.start
 
 /docker-entrypoint.sh
-
